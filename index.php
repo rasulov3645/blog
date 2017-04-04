5b1398853494eb95fa74fc79f83fbb60d7513c2a
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-04-05 00:57:21 +0300

    Урок 4. Основы PHP.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+    // echo "Hello world!";
+    // echo time();
+    
+    // $a = $_GET['a'];
+    // $b = $_GET['b'];
+    
+    // echo "a+b=".($a+$b)
+
+    // function add($param1, $param2) {
+    //    return $param1 + $param2; 
+    // }
+
+    //$a = $_GET['a'];
+    //$b = $_GET['b'];
+
+    //echo add($a, $b);
+
+
+    echo "<br>";
+    for($i=0; $i<10; $i++){
+        echo $i."<br>";
+    }
+     
+    
+?>
